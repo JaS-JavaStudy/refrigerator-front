515bf798dd9fb6d58c50f754ac276a2f2fc25b99
mincheol <kmc4889@naver.com>
2024-12-25 02:23:23 +0900

    [김민철] ..

--- src/pages/recipe/RecipeLiked.jsx
+++ src/pages/recipe/RecipeLiked.jsx
@@ -1,8 +1,8 @@
 import { useParams,useNavigate } from 'react-router-dom'
 import { useState,useEffect } from 'react'
 import { getLikedRecipes } from '@/sources/api/recipeAPI';
-import RecipeItem from '@/components/recipe/RecipeItem';
-import style from "@/assets/css/recipe/recipe.module.css"
+import RecipeItem from '@/components/recipe/recipeItem';
+import style from "@/assets/css/recipe/Recipe.module.css"
 function RecipeLiked() {
     const { userPk } = useParams()
     const [likedRecipeList, setLikedRecipeList] = useState([])
